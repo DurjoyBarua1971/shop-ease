40b83420d063083c91f2be288676f0f1bece8221
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-06-03 14:57:02 +0600

    Enhance cart page layout with improved empty cart message and continue shopping link; update cart item display structure.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useCart } from "../context/CartContext";
 import CartItemComponent from "../components/CartItem";
 import OrderSummary from "../components/OrderSummary";
+import Link from "next/link";
 
 function page() {
   const cartContext = useCart();
@@ -13,15 +14,26 @@ function page() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
       <div className="lg:col-span-2">
-        <h2 className="text-2xl font-bold mb-4">Your Cart</h2>
         {state.cartItems.length === 0 ? (
-          <p>Your cart is empty</p>
-        ) : (
-          <div className="space-y-2">
-            {state.cartItems.map((item) => (
-              <CartItemComponent key={item.id} item={item} />
-            ))}
+          <div className="flex justify-center items-center mx-auto gap-3 flex-col mt-16">
+            <h1 className="text-3xl font-bold">Your Cart is Empty</h1>
+            <p className="text-gray-400"> Add some products to get started!</p>
+            <Link
+              href="/products"
+              className="bg-blue-800 text-white px-6 font-semibold py-3 rounded-md hover:bg-blue-900 transition-colors mt-6"
+            >
+              Continue Shopping
+            </Link>
           </div>
+        ) : (
+          <>
+            <h2 className="text-2xl font-bold mb-4">Shopping Cart</h2>
+            <div className="space-y-2">
+              {state.cartItems.map((item) => (
+                <CartItemComponent key={item.id} item={item} />
+              ))}
+            </div>
+          </>
         )}
       </div>
       {state.cartItems.length !== 0 && <OrderSummary />}
