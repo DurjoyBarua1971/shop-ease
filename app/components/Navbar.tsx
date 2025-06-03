09732cd960550b275532978fd9f56a4381cd589c
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-06-03 17:29:58 +0600

    Enhance Navbar component to include cart item count; add error handling for CartContext availability.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,21 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { use } from "react";
 import { usePathname } from "next/navigation";
+import { useCart } from "../context/CartContext";
 
 function Navbar() {
   const pathname = usePathname();
+  const cartContext = useCart();
+  if (!cartContext) {
+    throw new Error("CartContext is not available");
+  }
+  const { state } = cartContext;
+  const itemCount = state.cartItems.reduce(
+    (count, item) => count + item.quantity,
+    0
+  );
+
   const isActive = (path: string) =>
     pathname === path
       ? "text-blue-500 font-semibold hover:text-blue-700 border-b-2 border-blue-400 pb-1"
@@ -21,6 +32,11 @@ function Navbar() {
         </Link>
         <Link href="/cart" className={isActive("/cart")}>
           🛒 Cart
+          {itemCount > 0 && (
+                <span className="absolute bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center font-semibold translate-x-33 -translate-y-8">
+                  {itemCount}
+                </span>
+              )}
         </Link>
       </div>
     </nav>
